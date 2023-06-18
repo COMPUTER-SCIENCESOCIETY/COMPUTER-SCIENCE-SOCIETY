21a41addc40758c94174bb81a559b3dbbe8ae44e
RishiBose961 <rishibose1901@gmail.com>
2023-06-18 21:06:16 +0530

    rishi update

--- client/src/components/HomeComponets/Footer.jsx
+++ client/src/components/HomeComponets/Footer.jsx
@@ -8,28 +8,38 @@ import {
 } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
+import Tooltip from "@mui/material/Tooltip";
+
 const Footer = () => {
   return (
     <div className="fixed  bottom-0 container mx-auto bg-gradient-to-r min-w-full from-cyan-500 h-8 to-blue-500 z-30">
       <div className="flex justify-around items-center mt-1">
         <div>
           <Link to="/">
-            <HomeIcon className="h-6 w-6 text-black cursor-pointer" />
+            <Tooltip title="Home">
+              <HomeIcon className="h-6 w-6 text-black cursor-pointer" />
+            </Tooltip>
           </Link>
         </div>
         <div>
           <Link to="/events">
-            <CalendarDaysIcon className="h-6 w-6 text-black cursor-pointer" />
+            <Tooltip title="Event">
+              <CalendarDaysIcon className="h-6 w-6 text-black cursor-pointer" />
+            </Tooltip>
           </Link>
         </div>
         <div>
           <Link to="/teams">
+          <Tooltip title="Team / Member">
             <UserGroupIcon className="h-6 w-6 text-black cursor-pointer" />
+            </Tooltip>
           </Link>
         </div>
         <div>
           <Link to="/gallery">
-            <PhotoIcon className="h-6 w-6 text-black cursor-pointer" />
+          <Tooltip title="Gallery">
+          <PhotoIcon className="h-6 w-6 text-black cursor-pointer" />
+          </Tooltip>
           </Link>
         </div>
       </div>
