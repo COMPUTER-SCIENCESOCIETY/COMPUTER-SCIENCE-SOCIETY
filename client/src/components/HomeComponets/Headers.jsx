edbc5129742454a4514e5f13f91ef01a74ace3ce
RishiBose961 <rishibose1901@gmail.com>
2023-08-19 11:50:28 +0530

    navbar update

--- client/src/components/HomeComponets/Headers.jsx
+++ client/src/components/HomeComponets/Headers.jsx
@@ -6,13 +6,16 @@ import { logout } from "../../slices/authSlices";
 import { useLogoutMutation } from "../../slices/userApiSlice";
 import LanguageSelect from "./LanguageSelect";
 import { useTranslation } from "react-i18next";
+import { Navbar } from "flowbite-react";
+import { FcCalendar, FcHome, FcLandscape } from "react-icons/fc";
+import { MdEmojiPeople } from "react-icons/md";
 
 const Headers = () => {
+  const { t } = useTranslation();
   const { userInfo } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [logoutApiCall] = useLogoutMutation();
-  const { t } = useTranslation();
 
   const logoutHandler = async () => {
     try {
@@ -24,16 +27,16 @@ const Headers = () => {
     }
   };
 
-
   return (
     <>
-      <nav className="bg-gradient-to-r from-cyan-500 to-blue-500">
-        <div className="mx-4 pt-1 pb-2 flex justify-between items-center">
+      <Navbar className="bg-gradient-to-r from-cyan-500 to-blue-500">
+        <Navbar.Brand>
           <div className="flex justify-start items-center">
             <img src={logo} alt="loading" className="w-16 h-16" />
+
             <Link to="/">
-              <p className=" capitalize font-bold">
-               {t("title")}
+              <p className=" capitalize font-bold hidden lg:flex">
+                {t("title")}
               </p>
             </Link>
             <img
@@ -42,31 +45,59 @@ const Headers = () => {
               className="w-16 h-16 mx-2"
             />
           </div>
-          <>
-            <div className="flex justify-center space-x-3">
-          <LanguageSelect/>
-             
-       
-              {userInfo ? (
-                <>
-                  <div className=" flex justify-start items-center mx-2">
-                    <p
-                      onClick={logoutHandler}
-                      className="mx-1 lg:mx-5 bg-red-400 cursor-pointer w-20 text-center rounded-full font-semibold hover:bg-slate-200"
-                    >
-                      Logout
-                    </p>
-                  </div>
-                </>
-              ) : (
-                <Link to="/auth/login">
-                  <p>Login</p>
-                </Link>
-              )}
-            </div>
-          </>
-        </div>
-      </nav>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Link
+            className="flex justify-center items-center space-x-3  m-3"
+            to="/"
+          >
+            <FcHome className="h-6 w-6 cursor-pointer" />
+          </Link>
+          <Link
+            className="flex justify-center items-center space-x-3 m-3"
+            to="/events"
+          >
+            <FcCalendar className="h-6 w-6  cursor-pointer" />
+            
+          </Link>
+
+          <Link
+            className="flex justify-center items-center space-x-3 m-3"
+            to="/teams"
+          >
+            <MdEmojiPeople className="h-6 w-6 cursor-pointer text-white" />
+            
+          </Link>
+
+          <Link
+            to="/gallery"
+            className="flex justify-center items-center space-x-3  m-3"
+          >
+            <FcLandscape className="h-6 w-6  cursor-pointer" />
+          
+          </Link>
+          <div  className="flex bg-amber-400 rounded-full pl-4 justify-center items-center">
+            {userInfo ? (
+              <>
+                <div className=" cursor-pointer hover:bg-red-400 rounded-lg ">
+                  <p
+                    onClick={logoutHandler}
+                    className="font-semibold text-black text-[14px] mx-1"
+                  >
+                    Logout
+                  </p>
+                </div>
+              </>
+            ) : (
+              <Link to="/auth/login">
+                <p>Login</p>
+              </Link>
+            )}
+            <LanguageSelect />
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 };
