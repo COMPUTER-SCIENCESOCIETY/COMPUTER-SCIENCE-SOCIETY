be782d47af9d1a8c7af36ec9a3a51f0b32fae52f
RishiBose961 <rishibose1901@gmail.com>
2023-08-20 22:26:11 +0530

    forgot password

--- controller/userController.js
+++ controller/userController.js
@@ -7,10 +7,9 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  const url = Math.floor(Math.random() * 9000);
-    sendEmailRegister(email, url, "Verify your Email");
+  // const url = Math.floor(Math.random() * 9000);
+  //   sendEmailRegister(email, url, "Verify your Email");
 
-    
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
 
@@ -100,54 +99,53 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email });
 
+    const url = Math.floor(Math.random() * 9000);
+    sendEmailRegister(email, url, "Verify your Email");
+
     if (!user) {
       return res.status(500).json({ success: false, message: "Invalid Eamil" });
     }
 
-    const url = Math.floor(Math.random() * 9000);
-    await sendEmailRegister(email, url, "Verify your Email");
-
-    user.resetPasswordOTP = url;
+    user.resetPasswordOTP = await url;
     user.resetPasswordotp_expiry = Date.now() + 10 * 60 * 1000;
 
-    await user.save();
+    await setTimeout(() => {
+      user.save();
+    }, 5000);
 
-    res.status(200).json({ success: true, message: `Otp Send To ${email}` });
+    res.status(200).json("OTP SENT");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-
-const resetPassword = asyncHandler(async(req,res)=>{
+const resetPassword = asyncHandler(async (req, res) => {
   try {
-
     const { otp, newPassword } = req.body;
 
     const user = await User.findOne({
-        resetPasswordOTP: otp,
-        resetPasswordotp_expiry: { $gt: Date.now() }
-    }).select("+password")
+      resetPasswordOTP: otp,
+      resetPasswordotp_expiry: { $gt: Date.now() },
+    }).select("+password");
 
     if (!user) {
-        res.status(200).json({ success: false, message: "Otp Expire" })
+      res.status(200).json({ success: false, message: "Otp Expire" });
     }
 
-    user.password = newPassword
-    user.resetPasswordOTP = null
-    user.resetPasswordotp_expiry = null
+    user.password = newPassword;
+    user.resetPasswordOTP = null;
+    user.resetPasswordotp_expiry = null;
 
-    await user.save()
+    await user.save();
 
-
-    res.status(200).json({ success: true, message: "Password Updated" })
-} catch (error) {
+    res.status(200).json({ success: true, message: "Password Updated" });
+  } catch (error) {
     res.status(500).json({
-        success: false,
-        message: error.message
-    })
-}
-})
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 export {
   authUser,
@@ -156,5 +154,5 @@ export {
   getUserProfile,
   updateUserProfile,
   forgotPassword,
-  resetPassword
+  resetPassword,
 };
