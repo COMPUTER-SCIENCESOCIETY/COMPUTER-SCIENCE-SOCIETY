4a564f7984e3501368c817b789b901d18f00cc46
RishiBose961 <rishibose1901@gmail.com>
2023-08-21 08:55:14 +0530

    otp issue

--- client/src/pages/Auth/ForgotScreen.jsx
+++ client/src/pages/Auth/ForgotScreen.jsx
@@ -33,81 +33,108 @@ const ForgotScreen = () => {
   //   }
   // };
 
-  const navigate = useNavigate();
-  const [data, setData] = React.useState(initialState);
-  const [Loading, setLoading] = useState(true);
-  const { email } = data;
+  // const navigate = useNavigate();
+  // const [data, setData] = React.useState(initialState);
+  // const [Loading, setLoading] = useState(true);
+  // const { email } = data;
 
-  const handleChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+  // const handleChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value });
+  // };
+
+  // const forgotpassword = async (e) => {
+  //   e.preventDefault();
+  //   //check field
+  //   if (isEmpty(email)) return alert("Please Fill in all fields");
+  //   if (!isEmail(email)) return alert("Please Enter a Valid Email Address");
+  //   try {
+  //     await axios.post("/api/users/forgotpassword", {
+  //       email,
+  //     });
+  //     setLoading(false);
+  //   } catch (error) {
+  //     alert(error.response.data.message);
+  //   }
+  // };
+
+  const [email, setEmail] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  const setVal = (e) => {
+    setEmail(e.target.value);
   };
 
-  const forgotpassword = async (e) => {
+  const sendLink = async (e) => {
     e.preventDefault();
-    //check field
-    if (isEmpty(email)) return alert("Please Fill in all fields");
-    if (!isEmail(email)) return alert("Please Enter a Valid Email Address");
-    try {
-      await axios.post("/api/users/forgotpassword", {
-        email,
+
+    if (email === "") {
+      alert("email is required!");
+    } else {
+      const res = await fetch("/api/users/forgotpassword", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
       });
-      setLoading(false);
-    } catch (error) {
-      alert(error.response.data.message);
+
+      const data = await res.json();
+
+      if (data.status == 201) {
+        setEmail("");
+        setMessage(true);
+      }
     }
   };
 
   return (
     <div>
-      {Loading ? (
-        <section>
-          <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-              <div className="mb-2 flex justify-center">
-                <img src="/itsl.png" alt="logo" className="w-20 h-20" />
-              </div>
-              <h2 className="text-center text-2xl font-bold leading-tight text-black">
-                Forgot Password
-              </h2>
+      <section>
+      {message ? <p style={{ color: "green", fontWeight: "bold" }}>pasword reset link send Succsfully in Your Email</p> : ""}
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+            <div className="mb-2 flex justify-center">
+              <img src="/itsl.png" alt="logo" className="w-20 h-20" />
+            </div>
+            <h2 className="text-center text-2xl font-bold leading-tight text-black">
+              Forgot Password
+            </h2>
 
-              <form action="#" method="POST" className="mt-8">
-                <div className="space-y-5">
-                  <div>
-                    <label
-                      htmlFor=""
-                      className="text-base font-medium text-gray-900"
-                    >
-                      {" "}
-                      Email address{" "}
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        onChange={handleChange}
-                      ></input>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="button"
-                      onClick={forgotpassword}
-                      className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
-                    >
-                      Get started
-                    </button>
+            <form action="#" method="POST" className="mt-8">
+              <div className="space-y-5">
+                <div>
+                  <label
+                    htmlFor=""
+                    className="text-base font-medium text-gray-900"
+                  >
+                    {" "}
+                    Email address{" "}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="email"
+                      placeholder="Email"
+                      value={email} onChange={setVal} name="email"
+                    ></input>
                   </div>
                 </div>
-              </form>
-            </div>
+
+                <div>
+                  <button
+                    type="button"
+                    onClick={sendLink}
+                    className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                  >
+                    Get started
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-        </section>
-      ) : (
-        <PasswordReset />
-      )}
+        </div>
+      </section>
     </div>
   );
 };
